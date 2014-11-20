36ac64db105a5c818a84ee29a13337e6488241ac
Sven Nilsen <bvssvni@gmail.com>
2014-11-20 18:08:27 +0100

    Made `from_memory_alpha` a normal method

--- src/texture.rs
+++ src/texture.rs
@@ -5,7 +5,7 @@ use image::{
     ImageBuf,
     Rgba,
 };
-use texture_lib::{ FromMemoryAlpha, ImageSize };
+use texture_lib::ImageSize;
 
 /// Represents a texture.
 pub struct Texture {
@@ -97,6 +97,48 @@ impl Texture {
             handle: tex,
         }
     }
+   
+    /// Creates texture from memory alpha. 
+    pub fn from_memory_alpha<
+        C: gfx::CommandBuffer,
+        D: gfx::Device<C>
+    >(
+        device: &mut D,
+        buffer: &[u8],
+        width: u32,
+        height: u32,
+    ) -> Texture {
+        use std::cmp::max;
+        
+        let width = max(width, 1);
+        let height = max(height, 1);       
+ 
+        let mut pixels = Vec::new();
+        for alpha in buffer.iter() {
+            pixels.push(255);
+            pixels.push(255);
+            pixels.push(255);
+            pixels.push(*alpha);
+        }
+
+        let texture_info = gfx::tex::TextureInfo {
+            width: width as u16,
+            height: height as u16,
+            depth: 1,
+            levels: 1,
+            kind: gfx::tex::Texture2D,
+            format: gfx::tex::RGBA8,
+        };
+
+        let image_info = texture_info.to_image_info();
+        let texture = device.create_texture(texture_info).unwrap();
+        device.update_texture(&texture, &image_info,
+            pixels.as_slice())
+            .unwrap();
+        Texture {
+            handle: texture
+        }
+    }
 
     /// Updates the texture with an image.
     pub fn update<
@@ -136,44 +178,6 @@ impl ImageSize for Texture {
     fn get_size(&self) -> (u32, u32) {
         let info = self.handle.get_info();
         (info.width as u32, info.height as u32)
-    }
-}
-
-impl<D: gfx::Device<C>, C: gfx::CommandBuffer> FromMemoryAlpha<D> 
-for Texture {
-    fn from_memory_alpha(
-        device: &mut D,
-        buffer: &[u8],
-        width: u32,
-        height: u32,
-        f: |&mut D, Texture| -> Texture
-    ) -> Option<Texture> {
-        
-        let mut pixels = Vec::new();
-        for alpha in buffer.iter() {
-            pixels.push(255);
-            pixels.push(255);
-            pixels.push(255);
-            pixels.push(*alpha);
-        }
-
-        let texture_info = gfx::tex::TextureInfo {
-            width: width as u16,
-            height: height as u16,
-            depth: 1,
-            levels: 1,
-            kind: gfx::tex::Texture2D,
-            format: gfx::tex::RGBA8,
-        };
-
-        let image_info = texture_info.to_image_info();
-        let texture = device.create_texture(texture_info).unwrap();
-        device.update_texture(&texture, &image_info,
-            pixels.as_slice())
-            .unwrap();
-        Some(f(device, Texture {
-            handle: texture
-        }))
     }
 }
 
